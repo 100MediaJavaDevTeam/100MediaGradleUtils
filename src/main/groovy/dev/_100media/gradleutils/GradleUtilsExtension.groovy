9886ecfef594f5eea338b199bb35895c0fa161f6
SizableShrimp <sizableshrimp@sizableshrimp.me>
2022-08-06 22:54:03 -0500

    addJarJarDep helper method
    Took 17 minutes

--- src/main/groovy/dev/_100media/gradleutils/GradleUtilsExtension.groovy
+++ src/main/groovy/dev/_100media/gradleutils/GradleUtilsExtension.groovy
@@ -1,6 +1,7 @@
 package dev._100media.gradleutils
 
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.dsl.RepositoryHandler
 
 import javax.inject.Inject
@@ -54,5 +55,31 @@ class GradleUtilsExtension {
      */
     Closure get100MediaSnapshotsMaven() {
         return GradleUtils.get100MediaSnapshotsMaven(this.project)
+    }
+
+    void addJarJarDepNoRangePrefix(dep, version, versionRange, classifier = null, boolean deobf = true,
+                                   implConfiguration = 'implementation', jarJarConfiguration = 'jarJar') {
+        addJarJarDep(dep, version, versionRange, classifier, null, deobf, implConfiguration, jarJarConfiguration)
+    }
+
+    void addJarJarDep(dep, version, versionRange, classifier = null, prefixMcVersion = this.project.extensions.extraProperties.find('MC_VERSION'),
+                      boolean deobf = true, implConfiguration = 'implementation', jarJarConfiguration = 'jarJar') {
+        def suffix = classifier != null && !classifier.isEmpty() ? ":${classifier}" : ""
+        if (prefixMcVersion != null && !prefixMcVersion.isEmpty()) {
+            def commaIdx = versionRange.indexOf(',')
+            def length = versionRange.length()
+            if (commaIdx == -1)
+                commaIdx = length
+            def temp = "${versionRange.charAt(0)}" + prefixMcVersion + '-' + versionRange.substring(1, commaIdx)
+            if (commaIdx != length)
+                temp = temp + ',' + prefixMcVersion + '-' + versionRange.substring(commaIdx + 1)
+            versionRange = temp
+        }
+
+        project.dependencies {
+            def depNotation = "${dep}:${version}"
+            "${implConfiguration}"(deobf ? project.extensions.getByName("fg").deobf(depNotation) : depNotation)
+            "${jarJarConfiguration}"("${dep}:${versionRange}${suffix}")
+        }
     }
 }
